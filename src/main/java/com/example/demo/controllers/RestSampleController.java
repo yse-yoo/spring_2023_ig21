f379d69abdbb1f9e39bcab7dc43f86b8a93457be
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-01 11:11:52 +0900

    update

--- src/main/java/com/example/demo/controllers/RestSampleController.java
+++ src/main/java/com/example/demo/controllers/RestSampleController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -20,4 +21,11 @@ public class RestSampleController {
         return message;
     }
 
+    @GetMapping(value="/users/{userId}")
+    public String getUser(@PathVariable Long userId) {
+        String message = "User ID is " + userId;
+        return message;
+    }
+
+    
 }

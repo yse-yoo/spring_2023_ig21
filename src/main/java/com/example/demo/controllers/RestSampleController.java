f231a8f49878970a912842ce84dd1c08d4ba2e8e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-01 10:37:25 +0900

    update

--- src/main/java/com/example/demo/controllers/RestSampleController.java
+++ src/main/java/com/example/demo/controllers/RestSampleController.java
@@ -3,14 +3,13 @@ package com.example.demo.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class RestSampleController {
-    
-    @GetMapping(value="/hello")
+
+    // get または method と入力して、GetMappingを選んで自動入力して修正
+    @GetMapping(value = "/hello")
     public String index() {
-        return "Hello!!!"; 
+        return "Hello!!!";
     }
-        
 
 }

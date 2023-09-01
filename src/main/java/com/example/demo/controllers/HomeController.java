04ec6fa3ab22874f774af11bed8e4604c2c1aecf
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-01 11:28:06 +0900

    add HomeController

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+    
+    @GetMapping(value="/")
+    @ResponseBody
+    public String index() {
+        return "Top Page!";
+    }
+    
+}

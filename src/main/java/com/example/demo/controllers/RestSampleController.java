afe56314128a2c6f46013eb67ee2bcb80d22c403
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-01 10:59:32 +0900

    add bye()

--- src/main/java/com/example/demo/controllers/RestSampleController.java
+++ src/main/java/com/example/demo/controllers/RestSampleController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class RestSampleController {
@@ -10,6 +12,12 @@ public class RestSampleController {
     @GetMapping(value = "/hello")
     public String index() {
         return "Hello!!!";
+    }
+
+    @GetMapping(value="/bye")
+    public String bye(@RequestParam String param) {
+        String message = "Bye! " + param;
+        return message;
     }
 
 }

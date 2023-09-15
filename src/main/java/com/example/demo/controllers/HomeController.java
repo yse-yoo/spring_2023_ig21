3bb76ae2c6bad05d59df627ff3836c2b7cd2c879
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-15 14:03:42 +0900

    update

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@Controller
+@Controller("HomeController")
 public class HomeController {
     
     @GetMapping(value="/")
--- src/main/java/com/example/demo/controllers/admin/HomeController.java
+++ src/main/java/com/example/demo/controllers/admin/HomeController.java
@@ -2,12 +2,13 @@ package com.example.demo.controllers.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("AdminHomeController")
+@RequestMapping("/admin/")
 public class HomeController {
     
-    @GetMapping(value="/admin/")
+    @GetMapping("")
     public String index() {
         // templates/admin/index.html がレンダリング
         return "admin/index";
